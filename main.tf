bbc6576e0291527533d45dd849c0ad6e58f72be6
Rodrigo Barbosa <59710101+rodrigo210686@users.noreply.github.com>
2024-11-06 17:20:45 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+variable "project_name" {}
+variable "owner_email" {}
+
+resource "google_project" "project" {
+  name       = var.project_name
+  project_id = var.project_name
+  org_id     = "653492276671"
+  billing_account = "0153E0-DDEFFA-D32EE6"
+}
+
+resource "google_project_iam_member" "owner" {
+  project = google_project.project.project_id
+  role    = "roles/owner"
+  member  = "user:${var.owner_email}"
+}
